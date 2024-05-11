193cdc8e0650a036f0bbef58d63169f67b0b8b6d
oltiy <oltiyoni@gmail.com>
2024-05-11 20:28:21 +0300

    added pagination

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -2,22 +2,32 @@ import { ChangeDetectionStrategy, Component, OnChanges, input } from '@angular/c
 import { CountryDisplay } from '../geo.interface';
 import { DecimalPipe } from '@angular/common';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [MatTableModule, DecimalPipe],
+  imports: [MatTableModule, DecimalPipe, MatPaginatorModule],
   templateUrl: './results.component.html',
   styleUrl: './results.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ResultsComponent implements OnChanges {
   countries = input<CountryDisplay[] | null>();
-  dataSource?: MatTableDataSource<CountryDisplay>;
+  totalCountries: number = 0;
+  dataSource!: MatTableDataSource<CountryDisplay>;
+  dataSourceDisplay = new MatTableDataSource<CountryDisplay>([]);
+  displayedColumns: string[] = ['commonName', 'capital', 'currencies', 'languages', 'population', 'flags'];
 
   ngOnChanges(): void {
+    this.totalCountries = this.countries()?.length ?? 0;
     this.dataSource = new MatTableDataSource<CountryDisplay>(this.countries() ?? []);
+    this.dataSourceDisplay.data = this.dataSource.data.slice(0, 10);
   }
 
-  displayedColumns: string[] = ['commonName', 'capital', 'currencies', 'languages', 'population', 'flags'];
+  onPaginateChange(event: PageEvent) {
+    const startIndex = event.pageIndex * event.pageSize;
+    const endIndex = startIndex + event.pageSize;
+    this.dataSourceDisplay.data = this.dataSource.data.slice(startIndex, endIndex);
+  }
 }
